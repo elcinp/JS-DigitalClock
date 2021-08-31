b074cd2ac1cc35a38f833b5bf9fd035ca87bd166
elcinp <elcinpars1@gmail.com>
2021-08-31 16:58:34 +0300

    Javascript digital clock with date

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-function startTime(){
+function currentTime(){
     const today = new Date();
-    let hours = today.getHours();
-    let minutes = today.getMinutes();
-    let seconds = today.getSeconds();
+    let hours = today.getHours(); // get hours
+    let minutes = today.getMinutes(); // get minutes
+    let seconds = today.getSeconds(); // get seconds
+    let midday = (hours>=12) ? "PM" : "AM" // assigning AM/PM
+    hours = (hours == 0) ? 12 : ((hours>12) ? (hours - 12): hours); // assigning hour in 12-hour format
+    hours = checkTime(hours)
     minutes = checkTime(minutes);
     seconds = checkTime(seconds)
-    document.getElementById("txt").innerHTML = hours + ":" + minutes + ":" +seconds
-    setTimeout(startTime,1000)
+    document.getElementById("time").innerHTML = hours + ":" + minutes + ":" +seconds + "  " + midday
+
+    let months = ['January','February','March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+    let curWeekDay = days[today.getDay()]; // get day
+    let curDay = today.getDate(); //get date
+    let curMonth = months[today.getMonth()]; // get month
+    let curYear = today.getFullYear(); // get year
+    document.getElementById("date").innerHTML = curWeekDay + "," + curDay + " " + curMonth + " " + curYear ; // get full date
+
+    setTimeout(currentTime,1000); // setting timer
 }
 
 function checkTime(i){
